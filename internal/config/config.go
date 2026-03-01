e5495c03080f012d565d09ea6a382e6851907793
CromartyForth <glenn@gcampbell.co.uk>
2026-03-01 22:21:44 +0000

    added Read() to read from json and construct a struct

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,38 +3,75 @@ package config
 import (
 	"os"
 	"fmt"
+	"io"
+	"encoding/json"
 )
 
 
-const configFileName = ".gatorconfig.json"
+const configFileName = "/gatorconfig.json"
 
 // Export a Config struct that represents the JSON file structure, including struct tags.
 type Config struct {
-	UserName string `json:"userName"`
 	DbURL string `json:"db_url"`
+	UserName string
 }
 
 // Export a Read function that reads the JSON file found at ~/.gatorconfig.json and returns a Config struct. It should read the file from the HOME directory, then decode the JSON string into a new Config struct. I used os.UserHomeDir to get the location of HOME.
 func Read() Config {
-	home, err := os.UserHomeDir()
-		if err != nil {
-			return Config{}
-		}
-	fmt.Println(home)
-	
-	return Config{}
+	fullpath, err:= getConfigFilePath()
+	if err != nil {
+		fmt.Println("er")
+	}
+
+
+	// open file readonly
+	file, err := os.Open(fullpath)
+	if err != nil {
+		fmt.Printf("error opening file: %v", err)
+		return Config{}
+	}
+
+	// read file ?
+	data, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println("error reading file")
+		return Config{}
+	}
+
+	newConfig := Config{}
+	err = json.Unmarshal(data, &newConfig)
+	if err != nil {
+		fmt.Println("error parsing json into struct")
+		return Config{}
+	}
+
+	return newConfig
 }
 
 // Export a SetUser method on the Config struct that writes the config struct to the JSON file after setting the current_user_name field.
 func (c *Config) SetUser(userName string) {
-
+	c.UserName = userName
+	err := write(*c)
+	if err != nil {
+		fmt.Printf("error writing file: %v", err)
+	}
+	return
 }
 
-func getConfigFilePath() (string, error){
-	return "", nil
-}
 
 func write(cfg Config) error {
+	
+	
 	return nil
 }
 
+
+func getConfigFilePath() (string, error){
+	home, err := os.UserHomeDir()
+		if err != nil {
+			return "", err
+		}
+	fullpath := home + configFileName
+	fmt.Println(fullpath)
+	return fullpath, nil
+}
--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"github.com/CromartyForth/gator/internal/config"
 )
 
-//Read the config file.
-func main() {
-	
-	config.Read()
-}
-//Set the current user to "lane" (actually, you should use your name instead) and update the config file on disk.
 
-//Read the config file again and print the contents of the config struct to the terminal.
+func main() {
+	//Read the config file.
+	cnf := config.Read()
+	fmt.Println(cnf)
+
+	//Set the current user to "lane" (actually, you should use your name instead) and update the config file on disk.
+	cnf.SetUser("Bilbo Baggins")
+
+
+	//Read the config file again and print the contents of the config struct to the terminal.
+	fmt.Println()
+}
+
+
